d793bcf1652f3f21408b3482f9c2e936d1ff6fcf
Chloe <tran.m.chloe@gmail.com>
2023-06-05 19:58:26 -0400

    Add Heading component and test

---
+++ src/__tests__/components/generic/Heading.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react'
+import Heading from '../../../components/generic/Heading'
+
+describe('Heading', () => {
+    test('renders a heading with the correct title', () => {
+        // arrange
+        const title = 'Test Heading'
+      
+        // act
+        render(<Heading title={title} />)
+        const headingElement = screen.getByText(title)
+      
+        // assert
+        expect(headingElement).toBeInTheDocument()
+        expect(headingElement.textContent).toBe(title)
+    })
+})
