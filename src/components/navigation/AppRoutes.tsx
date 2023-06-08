785ab565ac670a7ad7fac9970864683df3e7da78
Chloe <tran.m.chloe@gmail.com>
2023-06-08 15:44:25 -0400

    Edit routing
    - Define routes that are not accessible for users that are not logged in by wrapping them with a 'PrivateRoute' component
    - Add redirects to '/home' if user is already logged in

---
+++ src/components/navigation/AppRoutes.tsx
@@ -0,0 +1,79 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { selectAuth } from '../../services/auth/authSlice'
+
+import ErrorPage from '../../pages/error-page/ErrorPage'
+import AuthorizationPage from '../../pages/authorization-page/AuthorizationPage'
+import DashboardPage from '../../pages/dashboard-page/DashboardPage'
+import PrivateRoute from './PrivateRoute'
+import PastResultsPage from '../../pages/past-results-page/PastResultsPage'
+import QuizPage from '../../pages/quiz-page/QuizPage'
+import ResultPage from '../../pages/result-page/ResultPage'
+import SettingsPage from '../../pages/settings-page/SettingsPage'
+
+const AppRoutes = () => {
+  const { token } = useSelector(selectAuth)
+  
+  return (
+    <div data-testid='app-routes'>
+      <Routes>
+        {/* Fallback route for handling 404 errors */}
+        <Route path="*" element={<ErrorPage />} />
+        {/* Redirect to /home if the user is logged in */}
+        <Route
+          path="/"
+          element={
+            token ? (
+              <Navigate to="/home" replace />
+            ) : (
+              <Navigate to="/auth" replace />
+            )
+          }
+        />
+        <Route path="/auth" element={<AuthorizationPage />} />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <DashboardPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/quiz"
+          element={
+            <PrivateRoute>
+              <QuizPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/result"
+          element={
+            <PrivateRoute>
+              <ResultPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/past-results"
+          element={
+            <PrivateRoute>
+              <PastResultsPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <PrivateRoute>
+              <SettingsPage />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </div>
+  )
+}
+
+export default AppRoutes
--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createHashRouter, RouterProvider } from 'react-router-dom'
+import { store } from './app/redux-store'
+import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom'
 import './assets/css/index.css'
 import App from './App'
-import ErrorPage from './pages/error-page/ErrorPage'
-import routes from './routes'
-
-// Helper to generate child routes based on routes.tsx
-const generateChildRoutes = () => {
-  return routes.map((route) => {
-    return {
-      path: route.path,
-      element: route.element
-    }
-  })
-}
-
-// Create a hash router with the root route and child routes
-const router = createHashRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: generateChildRoutes()
-  }
-])
 
 // Create a root element to render the app inside of
 const root = ReactDOM.createRoot(
@@ -32,7 +11,9 @@ const root = ReactDOM.createRoot(
 )
 
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
 )
