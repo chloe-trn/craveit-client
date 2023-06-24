21ec48dc09221bc510ded1e59126b42aec025018
Chloe <tran.m.chloe@gmail.com>
2023-06-24 17:39:36 -0400

    Add quiz reducer to store

--- src/app/redux-store.ts
+++ src/app/redux-store.ts
@@ -3,13 +3,15 @@ import { setupListeners } from '@reduxjs/toolkit/query/react'
 import { tasteBudClientApi } from '../services/tasteBudClientApi'
 import authReducer from '../services/auth/authSlice'
 import menuReducer from '../services/menu/menuSlice'
+import quizReducer from '../services/quiz/quizSlice'
 
 // Configure Redux store
 export const store = configureStore({
   reducer: {
     [tasteBudClientApi.reducerPath]: tasteBudClientApi.reducer,
     auth: authReducer,
-    menu: menuReducer
+    menu: menuReducer,
+    quiz: quizReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(tasteBudClientApi.middleware),
