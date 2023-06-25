6bc146094b93c656caa41332dd21f90a8c938113
Chloe <tran.m.chloe@gmail.com>
2023-06-25 11:42:54 -0400

    Simplify the quiz component

--- src/pages/quiz-page/QuizPage.tsx
+++ src/pages/quiz-page/QuizPage.tsx
@@ -6,12 +6,12 @@ import { RootState } from '../../app/redux-store'
 import { useSubmitQuizMutation } from '../../services/tasteBudClientApi'
 import { setQuizBody } from '../../services/quiz/quizBody'
 import { clearQuiz } from '../../services/quiz/quizSlice'
+import { setLoadingWithDelay } from '../../helpers/loading-helper'
 
 import Loading from '../../components/navigation/loading/Loading'
 import ProgressBar from '../../components/navigation/progress-bar/ProgressBar'
 import QuizSubmissionError from '../error-pages/QuizSubmissionError'
 
-import Address from './questions/Address'
 import AddressMap from './questions/AddressMap'
 import Distance from './questions/Distance'
 import Cuisine from './questions/Cuisine'
@@ -26,30 +26,27 @@ const QuizPage = () => {
   const [currentQuestion, setCurrentQuestion] = useState(1)
   const [isQuizCompleted, setIsQuizCompleted] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
-  const [isAsyncCallCompleted, setIsAsyncCallCompleted] = useState(false)
   const [isError, setIsError] = useState(false)
   const [error, setError] = useState('')
 
-  // reset quiz state and form
+  // reset quiz states
   const handleReset = () => {
     setCurrentQuestion(1)
     setIsQuizCompleted(false)
     setIsLoading(false)
-    setIsAsyncCallCompleted(false)
     setIsError(false)
     setError('')
     dispatch(clearQuiz())
   }
 
-  // progress to previous and next quiz questions
+  // go to previous and next quiz questions
   const nextPage = () => setCurrentQuestion(currentQuestion + 1)
   const previousPage = () => setCurrentQuestion(currentQuestion - 1)
 
   // mutation hook for submitting quiz
-  const [submitQuiz, { data: quizData, isSuccess: isSubmitQuizSuccess, isError: isSubmitQuizError, error: submitQuizError }]
-   = useSubmitQuizMutation()
+  const [submitQuiz, { data: quizData, isSuccess: isSubmitQuizSuccess, isError: isSubmitQuizError, error: submitQuizError }] = useSubmitQuizMutation()
 
-   // handle quiz submission
+  // handle quiz submission
   const handleSubmit = () => setIsQuizCompleted(true)
 
   // on component load, start quiz from a blank state
@@ -61,8 +58,6 @@ const QuizPage = () => {
   // submit form values to get a quiz result
   useEffect(() => {
     const submitFormValues = async () => {
-
-      // prepare the request body
       let fullStreet
 
       if (quizState.location.streetContinued !== '') {
@@ -82,28 +77,8 @@ const QuizPage = () => {
         priceRange: quizState.priceRange.join(','),
       }
 
-      // save the body for later access
-      setQuizBody(body)
-
-      // set loading component while waiting for an api response
-      setIsLoading(true)
-      const startTime = Date.now()
-
-      await submitQuiz(body) // async call to the api
-
-      // if the server responds faster than the delay time
-      // the loading component will be shown for at least the delay time
-      const endTime = Date.now()
-      const delay = 2500
-      const remainingTime = delay - (endTime - startTime)
-  
-      if (remainingTime > 0) {
-        await new Promise((resolve) => setTimeout(resolve, remainingTime))
-      }
-      
-      // set states after the response is finished
-      setIsLoading(false)
-      setIsAsyncCallCompleted(true)
+      setQuizBody(body) 
+      await setLoadingWithDelay(async () => {await submitQuiz(body)}, setIsLoading)
     }
 
     if (isQuizCompleted) {
@@ -113,26 +88,21 @@ const QuizPage = () => {
   
    // handle quiz submission success
   useEffect(() => {
-    if (isSubmitQuizSuccess && isAsyncCallCompleted) {
+    if (isSubmitQuizSuccess && !isLoading) {
       handleReset()
       navigate('/result', { state: { quizData } })
     }
-  }, [isSubmitQuizSuccess, isAsyncCallCompleted])
+  }, [isSubmitQuizSuccess, isLoading])
 
   // handle quiz submission error
   useEffect(() => {
-    const handleQuizError = () => {
+    if (isSubmitQuizError) {
       if (submitQuizError && 'data' in submitQuizError) {
         setError(submitQuizError.data as string)
       } else {
         setError('SERVER ERROR')
       }
-
       setIsError(true)
-    }
-    
-    if (isSubmitQuizError) {
-      handleQuizError()
     }
   }, [isSubmitQuizError, submitQuizError])
 
