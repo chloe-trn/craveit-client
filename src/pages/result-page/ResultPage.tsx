4dbcd94b49fd4f3838be6c81d3ee8b5600b09f56
Chloe <tran.m.chloe@gmail.com>
2023-06-26 19:28:54 -0400

    Add save result button
    - Edit yelp button to yelp link
    - Replace yelp button with save result button
    - Implement save result functionality

--- src/pages/result-page/ResultPage.tsx
+++ src/pages/result-page/ResultPage.tsx
@@ -2,14 +2,16 @@ import './result.css'
 import { useEffect, useState } from 'react'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
-import { useSubmitQuizMutation } from '../../services/tasteBudClientApi'
+import { useSaveResultMutation, useSubmitQuizMutation } from '../../services/tasteBudClientApi'
 import { clearQuiz } from '../../services/quiz/quizSlice'
 import { getQuizBody } from '../../services/quiz/quizBody'
+import { setLoadingWithDelay } from '../../helpers/loading-helper'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPhone, faTags } from '@fortawesome/free-solid-svg-icons'
+import { faPhone, faTags, faGlobe, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 
 import Loading from '../../components/navigation/loading/Loading'
+import Message from '../../components/generic/Message'
 import QuizSubmissionError from '../error-pages/QuizSubmissionError'
 import StarRating from '../../components/misc/StarRating'
 import InteractiveMap from '../../components/maps/InteractiveMap'
@@ -20,17 +22,27 @@ const ResultPage = () => {
   const dispatch = useDispatch()
 
   // state variables
-  const [quizDataReturned, setQuizDataReturned] = useState(location.state?.quizData)
+  const [quizDataReturned, setQuizDataReturned] = useState(location.state?.quizData.business)
+  const [quizId, setQuizId] = useState(location.state?.quizData.quizId)
+
   const [isLoading, setIsLoading] = useState(false)
+
   const [error, setError] = useState('')
   const [isError, setIsError] = useState(false)
-  const [hasErrorRendered, setHasErrorRendered] = useState(false) // Flag to track error rendering
+
+  const [isSaveClicked, setIsSaveClicked] = useState(false)
+  const [isSaveError, setIsSaveError] = useState(false)
+  const [saveResponse, setSaveResponse] = useState('')
 
   // convert distance provided in km to mile
   const distanceInMiles = quizDataReturned ? (quizDataReturned.distance / 1609.34).toFixed(2) : null
+
+  // mutation hooks for saving result and submitting another quiz
+  const [saveResult, { isSuccess: isSaveResultSuccess, isError: isSaveResultError, error: saveResultError }] = useSaveResultMutation();
+
+  const [submitQuiz, { data: quizData, isSuccess: isSubmitQuizSuccess, isError: isSubmitQuizError, error: submitQuizError }] = useSubmitQuizMutation()
   
-  // reset quiz state and result page
-  // redirect user back to quiz page
+  // reset quiz state and result page, redirect user back to quiz page
   const handleReset = () => {
     setIsLoading(false)
     setError('')
@@ -39,43 +51,7 @@ const ResultPage = () => {
     navigate('/quiz')
   }
 
-  // mutation hook for submitting quiz
-  const [submitQuiz, { data: quizData, isSuccess: isSubmitQuizSuccess, isError: isSubmitQuizError, error: submitQuizError },] 
-  = useSubmitQuizMutation()
-
-   // submit stored form values from the most recent quiz
-   // to get another quiz result
-  const handleFindAnother = async () => {
-    // Pull quizState from cache 
-    const body = getQuizBody()
-
-    if (body !== null) {
-      // set loading component while waiting for an api response
-      setIsLoading(true)
-      const startTime = Date.now()
-
-      await submitQuiz(body) // async call to the api
-
-      // if the server responds faster than the delay time
-      // the loading component will be shown for at least the delay time
-      const endTime = Date.now()
-      const delay = 2500
-      const remainingTime = delay - (endTime - startTime)
-
-      if (remainingTime > 0) {
-        await new Promise((resolve) => setTimeout(resolve, remainingTime))
-      }
-
-      // set state after the response is finished
-      setIsLoading(false)
-    } else {
-      setError('This request cannot be made, please try again')
-      setIsError(true)
-    }
-  }
-
-  // when get directions is clicked, 
-  // prepare data and open google map link
+  // when get directions is clicked, prepare data and open google map link
   const handleGetDirections = () => {
     const { location } = quizDataReturned
     const address = `${location.address1}, ${location.city}, ${location.state}`
@@ -84,22 +60,75 @@ const ResultPage = () => {
     window.open(directionsLink, '_blank')
   }
 
+  // makes a call to the server to save the current result
+  const handleSave = async () => {
+    if (quizDataReturned && distanceInMiles){
+
+      const resultEntry = {
+        userId: '', 
+        quizId: quizId,
+        date: new Date().toISOString(),
+        restaurantName: quizDataReturned.name,
+        location: `${quizDataReturned.location.address1} ${quizDataReturned.location.city} ${quizDataReturned.location.state}`,
+        priceRange: quizDataReturned.price,
+        distance: distanceInMiles, 
+        rating: quizDataReturned.rating
+      }
+
+      await setLoadingWithDelay(async () => {await saveResult(resultEntry)}, setIsLoading)
+
+      setIsLoading(false)
+      setIsSaveClicked(true)
+
+    }
+  }
+
+  // handle save result success
+   useEffect(() => {
+    if (isSaveResultSuccess) {
+      setSaveResponse('This craving has been saved.')
+      setIsSaveError(false)
+    }
+  }, [isSaveResultSuccess])
+
+  // handle save result error
+  useEffect(() => {
+    if (isSaveResultError) {
+      setSaveResponse('Something went wrong, please try again.')
+      setIsSaveError(true)
+    }
+  }, [isSaveResultError, saveResultError])
+
+   // submit most recent quiz again 
+  const handleFindAnother = async () => {
+    const body = getQuizBody()
+
+    if (body !== null) {
+      await setLoadingWithDelay(async () => {await submitQuiz(body)}, setIsLoading)
+    } else {
+      setError('This request cannot be made, please try again')
+      setIsError(true)
+    }
+    setIsSaveClicked(false)
+    setIsSaveError(false)
+  }
+
   // handle quiz submission success
   useEffect(() => {
     if (isSubmitQuizSuccess) {
-      setQuizDataReturned(quizData)
+      setQuizDataReturned(quizData.business)
     }
   }, [isSubmitQuizSuccess, quizData])
 
   // handle quiz submission error
   useEffect(() => {
     const handleQuizError = () => {
+      setIsError(true)
       if (submitQuizError && 'data' in submitQuizError) {
         setError(submitQuizError.data as string)
       } else {
         setError('SERVER ERROR')
       }
-      setIsError(true)
     }
 
     if (isSubmitQuizError) {
@@ -110,12 +139,11 @@ const ResultPage = () => {
   // show an error message if the user navigates to this page
   // without taking a quiz first
   useEffect(() => {
-    if (!quizDataReturned && !hasErrorRendered) { 
+    if (!quizDataReturned) { 
       setError('Please complete the questionnaire before we can find a matching restaurant for you')
       setIsError(true)
-      setHasErrorRendered(true)
     }
-  }, [quizDataReturned, hasErrorRendered])
+  }, [quizDataReturned])
 
   return (
     <section className='result'>
@@ -126,7 +154,14 @@ const ResultPage = () => {
         <QuizSubmissionError onReset={handleReset} text={error}/> 
         ) : (
         <>
-        <h2>Destination found <span>🎯</span></h2> 
+        {isSaveClicked && 
+          <Message 
+            className={`msg ${isSaveError ? 'error' : 'success'}`} 
+            icon={faCheckCircle} 
+            text={saveResponse} 
+          />
+        }
+        <h2>Your craving destination is found <span>🎯</span></h2> 
           <div className='result-wrapper'>
             <div className='column left'>
               <h3 className='result-heading'>{quizDataReturned?.name}</h3> 
@@ -150,10 +185,19 @@ const ResultPage = () => {
                   <a href={`tel:${quizDataReturned?.phone}`}>{quizDataReturned?.phone}</a>
                 </p>
               }
-              <div className='btn-wrapper'>
-                <Link className='btn secondary' to={quizDataReturned?.url} target='_blank' rel='noopener noreferrer'>
+              <p>
+                <span className='hidden'>Yelp link</span>
+                <FontAwesomeIcon icon={faGlobe} />
+                <Link className='link' to={quizDataReturned?.url} target='_blank' rel='noopener noreferrer'>
                   View on Yelp
                 </Link>
+              </p>
+              <div className='btn-wrapper'>
+                {!isSaveClicked || isSaveError ? (
+                    <button className='btn secondary' onClick={handleSave}>
+                      Save This Spot
+                    </button>
+                ) : null}
                 <button className='btn primary' onClick={handleFindAnother}>
                   Find Another Spot
                 </button>
@@ -162,7 +206,7 @@ const ResultPage = () => {
             <div className='column right'>
               <InteractiveMap 
                 longitude={quizDataReturned?.coordinates.longitude || 0} 
-                latitude={quizDataReturned?.coordinates.latitude || 0}/> 
+                latitude={quizDataReturned?.coordinates.latitude || 0}/>
               <button className='btn tertiary' onClick={handleGetDirections}>
                 Get Directions ({distanceInMiles} miles away)
               </button>
