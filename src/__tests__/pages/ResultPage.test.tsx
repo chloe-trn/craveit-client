4cda3e3ad383ec0c969f94658a045dc7526e20c4
Chloe <tran.m.chloe@gmail.com>
2023-06-28 09:42:45 -0400

    Add save button tests to result page test suite

--- src/__tests__/pages/ResultPage.test.tsx
+++ src/__tests__/pages/ResultPage.test.tsx
@@ -1,53 +1,119 @@
-import { render, screen } from '@testing-library/react'
+import { rest, RestRequest, ResponseComposition, RestContext } from 'msw'
 import { MemoryRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { store } from '../../app/redux-store'
+import { server } from '../../test-utils/mock-server'
 import { mockResultData } from '../../test-utils/mock-data'
+import { tasteBudClientApi } from '../../services/tasteBudClientApi'
 import ResultPage from '../../pages/result-page/ResultPage'
 
 jest.mock('mapbox-gl')
 
 describe('ResultPage', () => {
-  test('renders quiz data correctly when not null', () => {
-    // arrange and act
-    render(
-      <Provider store={store}>
-        <MemoryRouter initialEntries={[{ 
-          pathname: '/',
-          state: { quizData: mockResultData }
-        }]}>
-          <ResultPage />
-        </MemoryRouter>
-      </Provider>
-    )
 
-    // assert
-    expect(screen.getByText(mockResultData.business.name)).toBeInTheDocument()
-    expect(screen.getByText(mockResultData.business.rating.toString())).toBeInTheDocument()
-    expect(screen.getByText(mockResultData.business.phone)).toBeInTheDocument()
+  beforeAll(() => server.listen())
+  afterEach(() => {
+    server.resetHandlers()
+    store.dispatch(tasteBudClientApi.util.resetApiState())
+  })
+  afterAll(() => server.close())
 
-    expect(screen.getByText('test category 1,')).toBeInTheDocument()
-    expect(screen.getByText('test category 2')).toBeInTheDocument()
+  describe('quiz data populating on page', () => {
+    test('renders quiz data correctly when not null', () => {
+      // arrange and act
+      render(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={[{ 
+            pathname: '/',
+            state: { quizData: mockResultData }
+          }]}>
+            <ResultPage />
+          </MemoryRouter>
+        </Provider>
+      )
   
-    expect(screen.getByText('View on Yelp')).toBeInTheDocument()
-    expect(screen.getByText('Find Another Spot')).toBeInTheDocument()
-    expect(screen.getByText('Get Directions (2.41 miles away)')).toBeInTheDocument()
+      // assert
+      expect(screen.getByText(mockResultData.business.name)).toBeInTheDocument()
+      expect(screen.getByText(mockResultData.business.rating.toString())).toBeInTheDocument()
+      expect(screen.getByText(mockResultData.business.phone)).toBeInTheDocument()
+  
+      expect(screen.getByText('test category 1,')).toBeInTheDocument()
+      expect(screen.getByText('test category 2')).toBeInTheDocument()
+    
+      expect(screen.getByText('View on Yelp')).toBeInTheDocument()
+      expect(screen.getByText('Find Another Spot')).toBeInTheDocument()
+      expect(screen.getByText('Get Directions (2.41 miles away)')).toBeInTheDocument()
+    })
+
+    test('renders error message when quiz data is null', () => {
+      // arrange and act
+      render(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={[{ 
+            pathname: '/',
+            state: { quizData: null }
+          }]}>
+            <ResultPage />
+          </MemoryRouter>
+        </Provider>
+      )
+  
+      // assert category 1
+      expect(screen.getByText(/Please complete the questionnaire before we can find a matching restaurant for you/i)).toBeInTheDocument()
+    })
   })
 
-  test('renders error message when quiz data is null', () => {
-    // arrange and act
-    render(
-      <Provider store={store}>
-        <MemoryRouter initialEntries={[{ 
-          pathname: '/',
-          state: { quizData: null }
-        }]}>
-          <ResultPage />
-        </MemoryRouter>
-      </Provider>
-    )
+  describe('save spot', () => {
+    test('save spot successfully', async () => {
+      // arrange 
+      render(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={[{ 
+            pathname: '/',
+            state: { quizData: mockResultData }
+          }]}>
+            <ResultPage />
+          </MemoryRouter>
+        </Provider>
+      )
+  
+      // act
+      fireEvent.click(screen.getByText('Save This Spot'))
+      
+      // assert
+      expect(await screen.findByText(/loading.../i)).toBeInTheDocument()
+      expect(await screen.findByText('This craving has been saved.')).toBeInTheDocument() 
+      expect(screen.queryByText(/loading.../i)).not.toBeInTheDocument()
+    })
 
-    // assert category 1
-    expect(screen.getByText(/Please complete the questionnaire before we can find a matching restaurant for you/i)).toBeInTheDocument()
+    test('fails to save spot', async () => {
+      // arrange
+      server.use(
+        rest.post(`${process.env.REACT_DEV_SERVER_URL}/api/results`, (req: RestRequest, res: ResponseComposition<any>, ctx: RestContext) => {
+          return res(ctx.status(500))
+        }),
+      )
+  
+      render(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={[{ 
+            pathname: '/',
+            state: { quizData: mockResultData }
+          }]}>
+            <ResultPage />
+          </MemoryRouter>
+        </Provider>
+      )
+  
+      // act
+      fireEvent.click(screen.getByText('Save This Spot'))
+      
+      // assert
+      expect(await screen.findByText(/loading.../i)).toBeInTheDocument()
+      expect(await screen.findByText('Something went wrong, please try again.')).toBeInTheDocument() 
+      expect(screen.queryByText(/loading.../i)).not.toBeInTheDocument()
+    })
   })
 })
