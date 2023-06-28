84fe20ce8aa9b377debb7076a87879cc7460dd6b
Chloe <tran.m.chloe@gmail.com>
2023-06-28 15:02:08 -0400

    Add dashboard page

---
+++ src/__tests__/pages/DashboardPage.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '../../test-utils/render-util'
+import { store } from '../../app/redux-store'
+import { setUser } from '../../services/auth/authSlice'
+import DashboardPage from '../../pages/dashboard-page/DashboardPage'
+
+describe('DashboardPage', () => {
+  it('renders correct username in the text', () => {
+    // arrange
+    store.dispatch(setUser({ username: 'mock_username', token: 'mock_token', expiration: 'mock_expiration' }))
+    // act
+    render(<DashboardPage />)
+    // assert
+    expect(screen.getByText(`Hey there, mock_username`)).toBeInTheDocument()
+  })
+
+  it('button takes the user to "/questionnaire" page', () => {
+    // arrange
+    store.dispatch(setUser({ username: 'mock_username', token: 'mock_token', expiration: 'mock_expiration' }))
+    // act
+    render(<DashboardPage />)
+    const button = screen.getByText('Take the Questionnaire')
+    // assert
+    expect(button).toBeInTheDocument()
+    expect(button.closest('a')).toHaveAttribute('href', '/questionnaire')
+  })
+})
--- src/pages/dashboard-page/DashboardPage.tsx
+++ src/pages/dashboard-page/DashboardPage.tsx
@@ -1,8 +1,20 @@
-
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { selectAuth } from '../../services/auth/authSlice'
 
 const DashboardPage = () => {
+  const { username } = useSelector(selectAuth)
+
   return (
-    <div>DashboardPage</div>
+    <section className='home'>
+      <h2>Hey there, {username} <span>👋</span></h2>
+      <p>Welcome to Crave It, the app that helps you overcome indecision and find the perfect restaurant!</p>
+      <p>Say goodbye to endless options and dilemmas. Crave It narrows down your choices and presents you with one perfect restaurant recommendation.</p>
+      <p>Ready to discover your next favorite dining spot?</p>
+      <Link to="/questionnaire">
+        <button className="btn gradient">Take the Questionnaire</button>
+      </Link>
+  </section>
   )
 }
 
