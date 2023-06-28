4be1be38cc97f72fd4c63b9d3ecf03fd84cc0579
Chloe <tran.m.chloe@gmail.com>
2023-06-28 09:38:13 -0400

    Add mock endpoints: post login, register, quizzes, and responses
    - edit mock data to pass in

--- src/test-utils/mock-data.ts
+++ src/test-utils/mock-data.ts
@@ -1,6 +1,23 @@
 import { Result } from '../helpers/types'
 
-// mocks of api responses for testing 
+// mock data for testing 
+
+export const mockLoginUserResponse = {
+  token: '1234',
+  expiration: '2023-06-25T19:01:05Z',
+  status: 'Success',
+  message: 'Login successful!',
+  username: 'mockusername'
+}
+
+export const mockRegisterUserResponse = {
+  token: '1234',
+  expiration: '2023-06-28T00:36:36Z',
+  status: 'Success',
+  message: 'User created successfully!',
+  username: 'mockusername'
+}
+
 export const mockQuizState = {
   location: {
     street: '',
@@ -12,6 +29,45 @@ export const mockQuizState = {
   distance: '',
   cuisine: [],
   priceRange: [],
+}
+
+export const mockSubmitQuizResponse = {
+  quizId: 1237,
+  business: {
+      id: 'abc',
+      alias: 'test-alias',
+      name: 'Test name',
+      imageUrl: null,
+      isClosed: false,
+      url: 'www.test.com',
+      reviewCount: 0,
+      categories: [
+          {
+              alias: 'alias-category',
+              title: 'title-category'
+          }
+      ],
+      rating: 4.5,
+      coordinates: {
+          latitude: 38.862179,
+          longitude: -77.295982
+      },
+      transactions: [],
+      price: '$$$',
+      location: {
+          address1: '42 Wallaby Way',
+          address2: 'Ste 100',
+          address3: '',
+          city: 'New York',
+          zipCode: null,
+          country: 'US',
+          state: 'NY',
+          displayAddress: null
+      },
+      phone: '+15715912955',
+      displayPhone: null,
+      distance: 10040.584
+  }
 }
 
 export const mockResultData = {
@@ -29,31 +85,40 @@ export const mockResultData = {
       longitude: 0,
       latitude: 0,
     },
-    distance: 3873
+    location: {
+      address1: '42 Wallaby Way',
+      city: 'New York',
+      zipCode: null,
+      country: 'US',
+      state: 'NY',
+      displayAddress: null,
+    },
+    price: '$$$',
+    distance: 3873,
   }
 }
 
-export const mockResults: Result[] = [
+export const mockGetResultsResponse: Result[] = [
   {
-    "id": 3093,
-    "userId": "ef242dbc-873a-47bc-9957-dafc6dbfabde",
-    "quizId": 3196,
-    "date": "2023-06-27T00:47:22.068",
-    "restaurantName": "Mock Restaurant Name 1",
-    "location": "13952 Lee Jackson Memorial Hwy Chantilly VA",
-    "priceRange": "$$",
-    "distance": "1.71",
-    "rating": 3 
+    id: 3093,
+    userId: '1234',
+    quizId: 3196,
+    date: '2023-06-27T00:47:22.068',
+    restaurantName: 'Mock Restaurant Name 1',
+    location: '42 Wallaby Way New York NY',
+    priceRange: '$$',
+    distance: '1.71',
+    rating: 3 
   },
   {
-    "id": 3094,
-    "userId": "ef242dbc-873a-47bc-9957-dafc6dbfabde",
-    "quizId": 3196,
-    "date": "2023-06-27T00:47:26.921",
-    "restaurantName": "Mock Restaurant Name 2",
-    "location": "12158 Fairfax Towne Ctr Fairfax VA",
-    "priceRange": "$$",
-    "distance": "5.48",
-    "rating": 4
+    id: 3094,
+    userId: 'ef242dbc-873a-47bc-9957-dafc6dbfabde',
+    quizId: 3196,
+    date: '2023-06-27T00:47:26.921',
+    restaurantName: 'Mock Restaurant Name 2',
+    location: '52 Wallaby Way New York NY',
+    priceRange: '$$',
+    distance: '5.48',
+    rating: 4
   }
 ]
--- src/test-utils/mock-server.ts
+++ src/test-utils/mock-server.ts
@@ -1,14 +1,34 @@
 import { rest, RestRequest, ResponseComposition, RestContext } from 'msw';
 import { setupServer } from 'msw/node';
-import { mockResults } from './mock-data'
+import { 
+  mockLoginUserResponse, 
+  mockRegisterUserResponse, 
+  mockSubmitQuizResponse,
+  mockGetResultsResponse 
+} from './mock-data'
 
 // set up mock server for testing
 
 const baseUrl = process.env.REACT_DEV_SERVER_URL
 
 export const handlers = [
+  rest.post(`${baseUrl}/api/user/login`, (req: RestRequest, res: ResponseComposition<any>, ctx: RestContext) => {
+    return res(
+      ctx.status(200),
+      ctx.json(mockLoginUserResponse)
+    )
+  }),
+  rest.post(`${baseUrl}/api/user/register`, (req: RestRequest, res: ResponseComposition<any>, ctx: RestContext) => {
+    return res(ctx.json(mockRegisterUserResponse))
+  }),
+  rest.post(`${baseUrl}/api/quizzes`, (req: RestRequest, res: ResponseComposition<any>, ctx: RestContext) => {
+    return res(ctx.json(mockSubmitQuizResponse))
+  }),
+  rest.post(`${baseUrl}/api/results`, (req: RestRequest, res: ResponseComposition<any>, ctx: RestContext) => {
+    return res(ctx.status(200))
+  }),
   rest.get(`${baseUrl}/api/results`, (req: RestRequest, res: ResponseComposition<any>, ctx: RestContext) => {
-    return res(ctx.json(mockResults));
+    return res(ctx.json(mockGetResultsResponse));
   }),
   rest.delete(`${baseUrl}/api/results/:id`, (req: RestRequest<{ id: string }>, res: ResponseComposition<any>, ctx: RestContext) => {
     return res(ctx.status(204));
