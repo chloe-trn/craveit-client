ac98bbf2a674d79df7c5cb44fe35f959a86dd8bc
Chloe <tran.m.chloe@gmail.com>
2023-06-28 09:43:19 -0400

    Add quiz progression test to quiz page test suite

--- src/__tests__/pages/quiz-page/QuizPage.test.tsx
+++ src/__tests__/pages/quiz-page/QuizPage.test.tsx
@@ -1,3 +1,4 @@
+import { fireEvent } from '@testing-library/react'
 import { render, screen } from '../../../test-utils/render-util'
 import QuizPage from '../../../pages/quiz-page/QuizPage'
 
@@ -19,5 +20,34 @@ describe('QuizPage', () => {
     expect(addressQuestion).toBeInTheDocument()
     expect(resetButton).toBeInTheDocument()
     expect(nextButton).toBeInTheDocument()
+  })
+
+  it('should progress through the quiz properly', () => {
+    // arrange and act
+    render(<QuizPage />)
+
+    // assert
+
+    // address question
+    fireEvent.change(screen.getByLabelText('Street Address'), { target: { value: '123 Test St' } })
+    fireEvent.change(screen.getByLabelText('City'), { target: { value: 'Test City' } })
+    fireEvent.change(screen.getByLabelText('State'), { target: { value: 'Test State' } })
+    fireEvent.change(screen.getByLabelText('Zip Code'), { target: { value: '12345' } })
+    fireEvent.click(screen.getByText('Next'))
+
+    // distance question
+    fireEvent.click(screen.getByLabelText('10 miles'))
+    fireEvent.click(screen.getByText('Next'))
+
+    // cuisine question
+    fireEvent.click(screen.getByLabelText('Thai'))
+    fireEvent.click(screen.getByText('Next'))
+
+    // price range question 
+    fireEvent.click(screen.getByLabelText('$11-$30'))
+    const submitButton = screen.getByText('Submit')
+    expect(submitButton).toBeEnabled()
+
+    fireEvent.click(screen.getByText('Submit'))
   })
 })
