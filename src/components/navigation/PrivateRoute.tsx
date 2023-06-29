e67d5f4558a8b2220ca7b36924699b42b09f7903
Chloe <tran.m.chloe@gmail.com>
2023-06-29 17:51:24 -0400

    Edit type of 'children' prop to ReactNode

--- src/components/navigation/PrivateRoute.tsx
+++ src/components/navigation/PrivateRoute.tsx
@@ -1,13 +1,18 @@
+import { ReactNode } from 'react'
 import { useSelector } from 'react-redux'
 import { selectAuth } from '../../redux/slices/authSlice'
 import LoadingToRedirect from './LoadingToRedirect'
 
-const PrivateRoute = ({ children }: { children: any }) => {
+type PrivateRouteProps = {
+  children: ReactNode
+}
+
+const PrivateRoute = ({ children }: PrivateRouteProps) => {
   const { token } = useSelector(selectAuth)
 
   // Render children if the user is logged in
   // otherwise render the 'LoadingToRedirect' component
-  return token ? children : <LoadingToRedirect />
+  return token ? <>{children}</> : <LoadingToRedirect />
 }
 
 export default PrivateRoute
