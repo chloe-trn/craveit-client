d076197006fd9af0e1d6d321cceb925537e0ab28
Chloe <tran.m.chloe@gmail.com>
2023-08-01 17:05:40 -0400

    Update Message test

--- src/tests/components/generic/Message.test.tsx
+++ src/tests/components/generic/Message.test.tsx
@@ -1,12 +1,17 @@
 import { render, screen } from '@testing-library/react'
-import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import Message from '../../../components/generic/Message'
+
+jest.mock('@fortawesome/react-fontawesome', () => ({
+  FontAwesomeIcon: ({ icon }: { icon: any }) => (
+    <i data-testid='fa-icon' className={`fa-${icon}`} aria-hidden='true'></i>
+  )
+}))
 
 describe('Message', () => {
   it('should render the message with the correct text and icon', () => {
     // arrange
     const className = 'message'
-    const icon: IconProp = 'check-circle'
+    const icon = 'check-circle'
     const text = 'Mock message'
 
     // act
@@ -15,13 +20,11 @@ describe('Message', () => {
     // assert
     const messageElement = screen.getByText(text)
     expect(messageElement).toBeInTheDocument()
+    expect(messageElement).toHaveClass(className)
 
-    const iconElement = screen.queryAllByRole('img', { hidden: true })
+    const iconElement = screen.getByTestId('fa-icon')
     expect(iconElement).toBeInTheDocument()
     expect(iconElement).toHaveClass(`fa-${icon}`)
-    expect(iconElement).toHaveAttribute('aria-hidden', 'true')
-
-    const messageContainer = screen.getByRole('paragraph', { name: text })
-    expect(messageContainer).toHaveClass(className)
+    expect(iconElement).toHaveAttribute('aria-hidden', 'true')    
   })
 })
